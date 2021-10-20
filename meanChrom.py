6a71d6e97f7bc608683b2c420fa2a474beb18ef7
ganglyimp <titiski15@gmail.com>
2021-10-20 00:49:49 -0400

    Finished data aumenting. APPEARS to be working fine.

--- meanChrom.py
+++ meanChrom.py
@@ -7,19 +7,22 @@ import random
 import numpy
 torch.set_default_tensor_type(torch.FloatTensor)
 
-#img_dir = "./face_images/*.jpg"
-img_dir = "./face_images/image00000.jpg"
+img_dir = "./face_images/*.jpg"
 files = glob.glob(img_dir)
 data = []
 
 for fl in files:
     img = cv2.imread(fl)
-    data.append(img) #img.T
+    data.append(img) #img.T to get in project spec format
 
 # Load data into tensor of size nImages x Channels x Height x Width
     # nImages = number of images in the folder
     # Channels = 3 (RBG colors)
     # Height, Width = 128
+
+# Here's an issue. OpenCV expects images to be in format nImages x Height x Width x Channels
+    # Does it affect anything having the dimensions being out of order? 
+    # We at least need to use the OpenCV ordering for preprocessing.
 imgTens= torch.tensor(data)
 
 # Randomly shuffle the data using torch.randperm
@@ -28,35 +31,31 @@ imgTens = imgTens[index].view(imgTens.size())
 
 # AUGMENT YOUR DATA
 # Augment by a small factor such as 10 to reduce overfitting by using OpenCV to transform your original images
+# there must be a better way of doing this than what I have going on. This is just ugly.
 augImg = torch.cat((imgTens, imgTens, imgTens, imgTens, imgTens, imgTens, imgTens, imgTens, imgTens, imgTens), 0)
 
-#TEST
-currImg = imgTens[0]
-
-#Horizontal Flips
-cv2.imshow('Image', cv2.flip(currImg.numpy(), 1))
-cv2.waitKey(0)
-
-#Random Crops
-newX = numpy.random.randint(0, 64)
-newY = numpy.random.randint(0, 64)
-
-'''
 for i in range(imgTens.shape[0], augImg.shape[0]):
-    currImg = augImg[i]
+    currImg = augImg[i].numpy()
 
-    # horizontal flips
+    # horizontal flips - 50% chance
     if random.random() < 0.5:
-        currImg = torch.from_numpy(cv2.flip(currImg.numpy(), 0))
+        currImg = cv2.flip(currImg, 0)
 
-    # random crops
-    newX = numpy.random.randint(0, 128)
-    newY = numpy.random.randint(0, 128)
+    # random crops - crop size ranges from 64 to 128
+    cropSize = numpy.random.randint(64, 128)
+
+    newX = numpy.random.randint(0, cropSize)
+    newY = numpy.random.randint(0, cropSize)
+
+    cropped = currImg[newY: newY + cropSize, newX: newX + cropSize]
+    currImg = cv2.resize(cropped, (128, 128))
 
     # scalings of input RBG values by single scaler randomly chosen between [0.6, 1.0]
+    randScalar = random.uniform(0.6, 1)
+    for i in range(3):
+        currImg[:, :, i] = currImg[:, :, i] * randScalar
 
-    augImg[i] = currImg
-'''
+    augImg[i] = torch.from_numpy(currImg)
 
 # CONVERT YOUR IMAGES TO L * a * b * COLOR SPACE
     # image = cv2.imread('example.jpg')
