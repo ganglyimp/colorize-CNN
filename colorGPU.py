85a6d2a3fb84792ff8e45b26d53bc5be35067b67
Thaira Sommer <sommert@login2.ufhpc>
2021-11-05 20:46:43 -0400

    Finished putting on GPU

--- colorGPU.py
+++ colorGPU.py
@@ -1,12 +1,14 @@
 # LOAD THE DATASETS
-import cv2 
-import os
+
+import cv2
+import os 
 import glob
 import torch
 import random
 import numpy as np
-import cutorch
 torch.set_default_tensor_type(torch.FloatTensor)
+
+device = torch.device("cuda:0")
 
 print("Loading dataset...")
 
@@ -22,7 +24,7 @@ for fl in files:
     # nImages = number of images in the folder
     # Channels = 3 (RBG colors)
     # Height, Width = 128
-imgTens = torch.cuda(data)
+imgTens = torch.tensor(data)
 
 # Randomly shuffle the data using torch.randperm
 index = torch.randperm(imgTens.shape[0])
@@ -62,8 +64,8 @@ for i in range(augImg.shape[0]):
     augImg[i] = torch.from_numpy(cv2.cvtColor(augImg[i].numpy(), cv2.COLOR_BGR2LAB))
 
 # Input: Grey scale image (L channel only)
-normalGreyImg = torch.cuda.zeros(7500, 128, 128)
-chromValues = torch.cuda.zeros(7500, 2, 128, 128)
+normalGreyImg = torch.zeros(7500, 128, 128)
+chromValues = torch.zeros(7500, 2, 128, 128)
 for i in range(7500):
     LChan, AChan, BChan = cv2.split(augImg[i].numpy())
 
@@ -91,36 +93,36 @@ class Network(nn.Module):
         K = 3 #kernel size
 
         # Convolutional Layers
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=C, kernel_size=K, stride=2, padding=1).cuda()
-        self.conv2 = nn.Conv2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1).cuda()
-        self.conv3 = nn.Conv2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1).cuda()
-        self.conv4 = nn.Conv2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1).cuda()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=C, kernel_size=K, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1)
         self.conv5 = nn.Conv2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1).cuda()
         self.conv6 = nn.Conv2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1).cuda()
 
         # Scales weights by gain parameter
-        nn.init.xavier_uniform_(self.conv1.weight).cuda()
-        nn.init.xavier_uniform_(self.conv2.weight).cuda()
-        nn.init.xavier_uniform_(self.conv3.weight).cuda()
-        nn.init.xavier_uniform_(self.conv4.weight).cuda()
-        nn.init.xavier_uniform_(self.conv5.weight).cuda()
-        nn.init.xavier_uniform_(self.conv6.weight).cuda()
+        nn.init.xavier_uniform_(self.conv1.weight)
+        nn.init.xavier_uniform_(self.conv2.weight)
+        nn.init.xavier_uniform_(self.conv3.weight)
+        nn.init.xavier_uniform_(self.conv4.weight)
+        nn.init.xavier_uniform_(self.conv5.weight)
+        nn.init.xavier_uniform_(self.conv6.weight)
 
         # Batch Normalization 
-        self.norm1 = nn.BatchNorm2d(C).cuda()
-        self.norm2 = nn.BatchNorm2d(C).cuda()
-        self.norm3 = nn.BatchNorm2d(C).cuda()
-        self.norm4 = nn.BatchNorm2d(C).cuda()
-        self.norm5 = nn.BatchNorm2d(C).cuda()
-        self.norm6 = nn.BatchNorm2d(C).cuda()
+        self.norm1 = nn.BatchNorm2d(C)
+        self.norm2 = nn.BatchNorm2d(C)
+        self.norm3 = nn.BatchNorm2d(C)
+        self.norm4 = nn.BatchNorm2d(C)
+        self.norm5 = nn.BatchNorm2d(C)
+        self.norm6 = nn.BatchNorm2d(C)
 
         # Deconvolutional Layers
-        self.deconv1 = nn.ConvTranspose2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1).cuda()
-        self.deconv2 = nn.ConvTranspose2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1).cuda()
-        self.deconv3 = nn.ConvTranspose2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1).cuda()
-        self.deconv4 = nn.ConvTranspose2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1).cuda()
-        self.deconv5 = nn.ConvTranspose2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1).cuda()
-        self.deconv6 = nn.ConvTranspose2d(in_channels=C, out_channels=2, kernel_size=K, stride=2, padding=1).cuda()
+        self.deconv1 = nn.ConvTranspose2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1)
+        self.deconv4 = nn.ConvTranspose2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1)
+        self.deconv5 = nn.ConvTranspose2d(in_channels=C, out_channels=C, kernel_size=K, stride=2, padding=1)
+        self.deconv6 = nn.ConvTranspose2d(in_channels=C, out_channels=2, kernel_size=K, stride=2, padding=1)
 
     def forward(self, t):
         # (1) hidden conv layer
@@ -164,6 +166,7 @@ class Network(nn.Module):
         return t
 
 network = Network()
+network = network.to(device)
 
 # Divide dataset into two parts: 90% training, 10% testing
 inputMat = normalGreyImg.unsqueeze(1)
@@ -172,8 +175,14 @@ tenPercent = int(inputMat.size(dim=0) * 0.1)
 testInput = inputMat[0:tenPercent, :, :, :]
 trainInput = inputMat[tenPercent:, :, :, :]
 
+testInput = testInput.to(device)
+trainInput = trainInput.to(device)
+
 testLabels = chromValues[0:tenPercent, :, :, :]
 trainLabels = chromValues[tenPercent:, :, :, :]
+
+testLabels = testLabels.to(device)
+trainLabels = trainLabels.to(device)
 
 # Creating mini-batches
 batchSize = 10
@@ -199,8 +208,8 @@ for trainBatch in trainLoader:
     optimizer.step()
 
 print("Testing the network...")
-lastBatch = []
-lastPreds = []
+#lastBatch = []
+#lastPreds = []
 
 i = 0
 meanLoss = 0
@@ -216,9 +225,9 @@ for testBatch in testLoader:
     meanLoss += loss.item()
 
     #Merging predicted A*B* channels for colorized image output (last batch only)
-    if i == 74:
-        lastBatch = testBatch.numpy()
-        lastPreds = pred.detach().numpy()
+    #if i == 74:
+        #lastBatch = testBatch.numpy()
+        #lastPreds = pred.detach().numpy()
     
     i += 1
 
@@ -227,6 +236,7 @@ meanLoss = meanLoss / i
 print("Mean loss: ", meanLoss)
 
 # Combining input L* channel and predicted A* B* channels to produce colorized image
+'''
 coloredImg = np.zeros((10, 128, 128, 3))
 
 lastBatch = np.interp(lastBatch, (lastBatch.min(), lastBatch.max()), (0, 100))
@@ -244,3 +254,4 @@ for i in range(10):
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+'''
